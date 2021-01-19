61b28002cead13f7f4db6f1bf9bcc08ae654cda6
bagrusss <>
2021-01-19 03:22:03 +0300

    fix build

--- app/src/main/java/ru/bagrusss/weatherapp/ui/flow/home/screens/detailed_weather/DetailedWeatherFragment.kt
+++ app/src/main/java/ru/bagrusss/weatherapp/ui/flow/home/screens/detailed_weather/DetailedWeatherFragment.kt
@@ -1,21 +1,23 @@
 package ru.bagrusss.weatherapp.ui.flow.home.screens.detailed_weather
 
-import ru.bagrusss.arch.rx_mvi.common.IOData
-import ru.bagrusss.weatherapp.ui.flow.home.screens.detailed_weather.DetailedWeatherContract.InputData
-import ru.bagrusss.weatherapp.ui.flow.home.screens.detailed_weather.DetailedWeatherContract.UIState
 import ru.bagrusss.arch.common.unsafeLazy
 import ru.bagrusss.arch.rx_mvi.MviFragment
+import ru.bagrusss.arch.rx_mvi.common.IOData
+import ru.bagrusss.weatherapp.R
+import ru.bagrusss.weatherapp.di.AppProvider
+import ru.bagrusss.weatherapp.ui.flow.home.screens.detailed_weather.DetailedWeatherContract.InputData
+import ru.bagrusss.weatherapp.ui.flow.home.screens.detailed_weather.DetailedWeatherContract.UIState
 
 class DetailedWeatherFragment : MviFragment<UIState, InputData, IOData.EmptyOutput>(R.layout.fragment_detailed_weather) {
 
     override val viewModel by unsafeLazy { component.viewModel }
 
     override val component by unsafeLazy {
-        DiProvider.component
-                    .detailedWeatherComponentBuilder
-                    .inputData(inputData)
-                    .fragment(this)
-                    .build()
+        AppProvider.api
+            .detailedWeatherComponentBuilder
+            .inputData(inputData)
+            .fragment(this)
+            .build()
     }
 
     override fun buildScreen(state: UIState) {
--- app/src/main/java/ru/bagrusss/weatherapp/ui/flow/home/screens/map/MapFragment.kt
+++ app/src/main/java/ru/bagrusss/weatherapp/ui/flow/home/screens/map/MapFragment.kt
@@ -1,22 +1,23 @@
 package ru.bagrusss.weatherapp.ui.flow.home.screens.map
 
-import ru.bagrusss.arch.rx_mvi.common.IOData
-import ru.bagrusss.weatherapp.ui.flow.home.screens.map.MapContract.InputData
-import ru.bagrusss.weatherapp.ui.flow.home.screens.map.MapContract.UIState
 import ru.bagrusss.arch.common.unsafeLazy
 import ru.bagrusss.arch.rx_mvi.MviFragment
+import ru.bagrusss.arch.rx_mvi.common.IOData
 import ru.bagrusss.weatherapp.R
+import ru.bagrusss.weatherapp.di.AppProvider
+import ru.bagrusss.weatherapp.ui.flow.home.screens.map.MapContract.InputData
+import ru.bagrusss.weatherapp.ui.flow.home.screens.map.MapContract.UIState
 
 class MapFragment : MviFragment<UIState, InputData, IOData.EmptyOutput>(R.layout.fragment_map) {
 
     override val viewModel by unsafeLazy { component.viewModel }
 
     override val component by unsafeLazy {
-        DiProvider.component
-                    .mapComponentBuilder
-                    .inputData(inputData)
-                    .fragment(this)
-                    .build()
+        AppProvider.api
+            .mapComponentBuilder
+            .inputData(inputData)
+            .fragment(this)
+            .build()
     }
 
     override fun buildScreen(state: UIState) {
